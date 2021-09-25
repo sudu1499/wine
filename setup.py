bd0b74a41ba459439c6507c2c14665c516762480
sudu1499 <suduanagha@gmail.com>
2021-09-25 22:42:17 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='src',
+    version='0.0.1',
+    description='its demo for dvc',
+    author='sudu bhai',
+    packages=find_packages(),
+    license='MIT'
+)
---
+++ tests/test_config.py
@@ -0,0 +1,7 @@
+def test_fun():
+    a=10
+    b=20
+    assert a+10==b
+
+def test_fun_fun():
+    assert 2==2
